ceecd1c4371fc9dac7404fe620afbb39e8e95243
Vladimir <vladistar200302@gmail.com>
2021-12-31 17:54:41 -0500

    m: link cv

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const URL_CV = 'https://drive.google.com/file/d/1kYHeX-d3rQ3TwlhoE_vGkCFvQVGnv4n2/view?usp=sharing';
+export const URL_CV = 'https://drive.google.com/file/d/1sjEdl5qytJ6Vtk6UsEqWWv7QvbH_rzvs/view?usp=sharing';
 export const URL_Github = 'https://github.com/Vdymir'
