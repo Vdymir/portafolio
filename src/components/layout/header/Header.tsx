32ed6f3836f02064bce325150e79286a06231426
Vladimir <vladistar200302@gmail.com>
2022-01-13 16:49:36 -0500

    refactor: retructuracion

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,37 +1,33 @@
 import Navbar from '../Navbar'
 import { Button, Container, ContainerTxt, HeaderConatiner, Headers, Img } from './header.style'
-import profile from '../../../assets/profile.jpeg'
+import profile from '../../../assets/profileNew.jpeg'
 import { URL_CV, URL_Github } from '../../../config'
 import ScrollAnimation from 'react-animate-on-scroll';
 
-const Header = () => {
+const Header = () => (
+    <Headers>
+        <Navbar />
+        <HeaderConatiner>
+            <Container>
+                <ScrollAnimation animateIn="bounceInLeft" animateOut="bounceOutRight" delay={500}>
+                    <Img src={profile} alt='Profile' onClick={() => window.open(URL_Github)} />
+                </ScrollAnimation>
+            </Container>
+            <ContainerTxt>
+                <ScrollAnimation animateIn='flipInY' animateOut='flipOutY' delay={700}>
+                    <span>Hi! I'm 👋🏼</span>
+                    <h2>Vladimir Castañeda</h2>
+                    <p>Frontend Developer</p>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="bounceInRight" animateOut="bounceOutLeft" delay={800}>
+                    <Button onClick={() => window.open(URL_CV)} >
+                        ver cv
+                    </Button>
+                </ScrollAnimation>
+            </ContainerTxt>
+        </HeaderConatiner>
+    </Headers>
+);
 
-    const handleClick = (url: string) => window.open(url)
-
-    return (
-        <Headers>
-                <Navbar/>
-            <HeaderConatiner>
-                <Container>
-                    <ScrollAnimation animateIn="bounceInLeft" animateOut="bounceOutRight" delay={500}>
-                        <Img src={profile} alt='Profile' onClick={() => handleClick(URL_Github)} />
-                    </ScrollAnimation>
-                </Container>
-                <ContainerTxt>
-                    <ScrollAnimation animateIn='flipInY' animateOut='flipOutY' delay={700}>
-                        <span>Hi! I'm 👋🏼</span>
-                        <h2>Vladimir Castañeda</h2>
-                        <p>Frontend Developer</p>
-                    </ScrollAnimation>
-                    <ScrollAnimation animateIn="bounceInRight" animateOut="bounceOutLeft" delay={800}>
-                        <Button onClick={() => handleClick(URL_CV)} >
-                            ver cv
-                        </Button>
-                    </ScrollAnimation>
-                </ContainerTxt>
-            </HeaderConatiner>
-        </Headers>
-    )
-}
 
 export default Header
